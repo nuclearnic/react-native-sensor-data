f94d322331a63669b37a9437bb190382d7b45a6c
nuclearnic <nuclearnicdev@gmail.com>
2018-08-27 08:35:39 +0200

    update README and try out some libraries

---
+++ README.md
@@ -0,0 +1,15 @@
+# README
+
+## Adding Libraries
+* `$ npm install react-native-library --save`
+* `$ npm link`
+
+The following libraries have been added:
+* react-native-device-info
+* react-native-call-history (Potential issue with this one. Might be replaceable with another.)
+* react-native-network-info
+* react-native-calendar-events
+
+
+## Adding Permissions
+* Add to `AndroidManifest.xml
--- src/App.js
+++ src/App.js
@@ -1,8 +1,45 @@
 import React, { Component } from 'react'
 import { View, Text } from 'react-native'
 
+import CallHistory from 'react-native-call-history'
+import RNDeviceInfo from 'react-native-device-info'
+import NetworkInfo from 'react-native-network-info'
+import RNCalendarEvents from 'react-native-calendar-events'
+
 export default class App extends Component {
+  state={
+    endpoint: null,
+    callHistory: null,
+    location: {
+      latitude: null,
+      longitude: null,
+      error: null,
+    },
+    networkInfo: {
+      ip: null,
+      ipv4: null,
+      ssid: null,
+      bssid: null,
+    }
+  };
+
+
+
   render() {
+    // CallHistory.list(
+    //   (history) => {this.setState({callHistory: history})},
+    //   (error) => {console.warn(error)}
+    // )
+    console.log('Logging stuff')
+    console.log(RNDeviceInfo.getBrand())
+    // console.log(this.state.callHistory)
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        console.log(position)
+      },
+      (error) => this.setState({ error: position.error.message }),
+      { enableHighAccuracy: true, timeout: 20000 },
+    )
     return (
       <View>
         <Text>Hello World</Text>
